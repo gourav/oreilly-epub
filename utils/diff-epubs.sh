a4d8965250d2ebefc6476a19ead74b51fd610ed9
A Farzat <a@farzat.xyz>
2026-03-08 08:18:36 +0300

    Hide unnecessary output in diff-epubs.sh

--- utils/diff-epubs.sh
+++ utils/diff-epubs.sh
@@ -5,8 +5,8 @@
 
 tmp_dir="$(mktemp -d --suffix=-diff-epubs)"
 
-unzip "$1" -d "$tmp_dir/old"
-unzip "$2" -d "$tmp_dir/new"
+unzip "$1" -d "$tmp_dir/old" > /dev/null
+unzip "$2" -d "$tmp_dir/new" > /dev/null
 diff --strip-trailing-cr -r "$tmp_dir"/{old,new}
 exit_status=$?
 rm -rf "$tmp_dir"
